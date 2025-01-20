156cc560c49d02f67d5776669cbe278e417c25fc
HoiBro1 <finndegroot2008@gmail.com>
2025-01-20 22:20:59 +0100

    Added a complete heuristic algorithm.

--- Heuristic.py
+++ Heuristic.py
@@ -6,13 +6,14 @@ import numpy as np
 class Heuristic:
     """Class which can generate a strategy, using heuristic rules."""
 
-    def __init__(self, game, abstraction_function, confidence):
-        """Initialize a game, a dictionary containing the strategy profile and an abstraction function and confidence."""
+    def __init__(self, game, abstraction_function, std):
+        """Initialize a game, a dictionary containing the strategy profile, an abstraction function and a standard deviation."""
         self.game = game
         self.infoset_dict = {}
         self.abstraction_function = abstraction_function
         self.infoset_data = (self.infoset_dict, self.abstraction_function)
-        self.confidence = confidence
+        self.std = std
+        self.max_chs = self.get_hand_strength(0)
 
     def get_infoset(self, info_key):
         """Create an infoset if needed and return."""
@@ -34,10 +35,24 @@ class Heuristic:
         """Returns false if first player of round and last played card in the history if not"""
         if len(game_state[2]) % 2 == 0:
             return False
-        elif len(game_state[2]) > 1:
-            return game_state[2][-1]
         else:
-            return False
+            return game_state[2][-1]
+    
+    def get_hand_strength(self, player, game_state=None):
+        """Get the compared hand strength of a player. If no game state is given returns the maximum hand strength"""
+        deck = self.game.deck.deck2.copy()
+        if game_state:
+            hand = game_state[1][player]
+            for card in hand:
+                deck.remove(card)
+            deck.remove(game_state[1][2])
+        else:
+            deck.sort(key= lambda x: x[1], reverse=True)
+            hand = deck[:self.game.handsize]
+            deck = deck[self.game.handsize + 1:]
+        hand_strength = sum(map(lambda x: x[1], hand)) / len(hand)
+        deck_strength = sum(map(lambda x: x[1], deck)) / len(deck)
+        return hand_strength - deck_strength
 
     def heuristic(self, game_state):
         """Recursive function which updates the infosets. Use this function to define your heuristic strat"""
@@ -55,23 +70,18 @@ class Heuristic:
             infoset = self.get_infoset(info_key)
             react = self.reacting(game_state)
 
-            # Define rules using the info_key
+            # Define rules using the info key or game state
             # A passive player who plays highest card if winnable and lowest otherwise
+            # the player bets according to their hand strength compared to the deck strength
+            # with a normal distribution applied
             if len(game_state[2]) < 2:
-                hand = game_state[1][game_state[0]]
-                deck = self.game.deck.deck2.copy()
-                for card in hand:
-                    deck.remove(card)
-                deck.remove(game_state[1][2])
-                hand_strength = sum(map(lambda x: x[1], hand)) / len(hand)
-                deck_strength = sum(map(lambda x: x[1], deck)) / len(deck)
-                compared_hand_strength = hand_strength - deck_strength
-
-                ranks = len(self.game.deck.ranks)
-                
-                if compared_hand_strength > 0:
-                    index
-                index = possible_actions.index(max(possible_actions, key=lambda t: t[1]))
+                chs = self.get_hand_strength(game_state[0], game_state=game_state)
+                chs = (((self.game.handsize + 1)*chs/self.max_chs)+self.game.handsize)/2
+                index = round(random.normalvariate(chs, self.std))
+                if index < 0:
+                    index = 0
+                elif index > self.game.handsize:
+                    index = self.game.handsize
             elif react is not False and \
                     react[0] == possible_actions[0][0] and \
                     react[1] < max(possible_actions, key=lambda t: t[1])[1]:
@@ -88,6 +98,18 @@ class Heuristic:
 
             for action in possible_actions:
                 self.heuristic(self.game.get_next_game_state(game_state, action))
+    
+    def train_heuristic(self):
+        """Fully train the heuristic algorithm by calling it at every chance node."""
+        for trump in self.game.deck.ranks:
+            self.game.deck.reset_deck()
+            self.game.deck.deck1.remove((self.game.deck.suit[0], trump))
+            for dealt_cards in itertools.combinations(self.game.deck.deck1, self.game.handsize * 2):
+                for hand1 in itertools.combinations(dealt_cards, self.game.handsize):
+                    hand2 = list(card for card in dealt_cards if card not in hand1)
+                    hands = [sorted(list(hand1)), sorted(hand2), (self.game.deck.suit[0], trump)]
+                    game_state = self.game.sample_new_game(hands=hands)
+                    self.heuristic(game_state)
 
     def count_infosets(self):
         """Count total number of infosets in the infodictionary."""
